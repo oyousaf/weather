81190fd7d71916ff5b6a622717d1d652cb580343
Omar ♔ <o_yousaf@live.co.uk>
2024-01-07 15:19:42 +0000

    squashing bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.js",
+  ],
   theme: {
     extend: {},
   },
