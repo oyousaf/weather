fdb07c1d24ffa8ba43a32d45ea3f8242beed78e1
Omar ♔ <o_yousaf@live.co.uk>
2024-01-08 11:33:35 +0000

    squashing bugs

--- src/App.vue
+++ src/App.vue
@@ -40,8 +40,8 @@
         <br />
         <div>
           <p
-            class="text-lg font-bold italic"
-            :style="{ 'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.5)' }"
+            class="text-lg font-bold italic text-shadow-lg"
+            style="{ 'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.5)' }"
           >
             Feels like
           </p>
@@ -78,65 +78,4 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "app",
-  data() {
-    return {
-      api_key: process.env.VUE_APP_WEATHER_API_KEY,
-      url_base: "https://api.openweathermap.org/data/2.5/",
-      query: "",
-      weather: {},
-    };
-  },
-  methods: {
-    fetchWeather(e) {
-      if (e.key == "Enter") {
-        fetch(
-          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .then(this.setResults);
-      }
-    },
-    setResults(results) {
-      this.weather = results;
-    },
-    dateBuilder() {
-      let d = new Date();
-      let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-      let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-
-      return `${day} ${date} ${month} ${year}`;
-    },
-  },
-};
-</script>
+<script src="./script.js"></script>
---
+++ src/script.js
@@ -0,0 +1,67 @@
+export default {
+    name: "app",
+    data() {
+      return {
+        api_key: process.env.VUE_APP_WEATHER_API_KEY,
+        url_base: "https://api.openweathermap.org/data/2.5/",
+        query: "",
+        weather: {},
+      };
+    },
+    computed: {
+      isWarm() {
+        return (
+          typeof this.weather.main !== "undefined" &&
+          this.weather.main.temp > 16
+        );
+      },
+    },
+    methods: {
+      fetchWeather(e) {
+        if (e.key == "Enter") {
+          fetch(
+            `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+          )
+            .then((res) => res.json())
+            .then(this.setResults);
+        }
+      },
+      setResults(results) {
+        this.weather = results;
+      },
+      dateBuilder() {
+        let d = new Date();
+        let months = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+        let days = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
+  
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+  
+        return `${day} ${date} ${month} ${year}`;
+      },
+    },
+  };
+  
