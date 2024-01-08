7f15d04faeeabc883968891c2fc6276e9940e5a3
Omar ♔ <o_yousaf@live.co.uk>
2024-01-08 12:38:35 +0000

    squashing bugs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div
-    id="app"
-    :class="
-      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
-    "
-  >
+  <div id="app" :class="{ warm: isWarm }">
     <main>
       <div class="w-full mb-8">
         <input
@@ -20,14 +15,13 @@
         class="text-center items-center justify-center content-center text-white"
         v-if="weather.main"
       >
-        <div>
+        <!-- Location -->
           <div class="text-3xl font-medium text-shadow-md">
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
           <div class="text-2xl font-light italic">{{ dateBuilder() }}</div>
-        </div>
 
-        <div>
+        <!-- Temp -->
           <div
             class="inline-block items-center justify-center p-4 text-9xl font-bold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
           >
@@ -36,27 +30,30 @@
           <div class="text-7xl font-bold italic text-shadow-lg">
             {{ weather.weather[0].main }}
           </div>
-        </div>
         <br />
-        <div>
-          <p class="text-lg font-bold italic text-shadow-lg">Feels like</p>
-          <p
-            class="inline-block items-center justify-center p-4 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
-          >
-            {{ weather.main.feels_like.toFixed(0) }}°C
-          </p>
-          <p class="text-lg font-bold italic text-shadow-lg">Humidity</p>
-          <p
-            class="inline-block w-32 text-center items-center justify-center p-4 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
-          >
-            {{ weather.main.humidity }}%
-          </p>
-          <p class="text-lg font-bold italic text-shadow-lg">Winds</p>
-          <p
-            class="inline-block items-center justify-center p-4 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
-          >
-            {{ weather.wind.speed.toFixed(0) }} MPH
-          </p>
+
+        <!-- Feels like -->
+        <div class="text-2xl font-bold italic">Feels like</div>
+        <div
+          class="inline-block items-center justify-center p-4 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
+        >
+          {{ weather.main.feels_like.toFixed(0) }}°C
+        </div>
+
+        <!-- Humidity -->
+        <div class="text-2xl font-bold italic text-shadow-lg">Humidity</div>
+        <p
+          class="inline-block w-32 text-center items-center justify-center p-2 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
+        >
+          {{ weather.main.humidity }}%
+        </p>
+
+        <!-- Winds -->
+        <div class="text-2xl font-bold italic text-shadow-lg">Winds</div>
+        <div
+          class="inline-block items-center justify-center p-4 text-5xl font-extrabold text-shadow-lg bg-white/25 rounded-2xl my-8 shadow-lg"
+        >
+          {{ weather.wind.speed.toFixed(0) }} MPH
         </div>
       </div>
     </main>
